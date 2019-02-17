81084c40a08a7e38a0e2cbfdebb28171f4aebf83
Clement <clement.bisaillon@hotmail.com>
2019-02-17 08:23:59 -0500

    Début interface graphique

--- src/main/java/org/lrima/laop/core/LaopGraphical.java
+++ src/main/java/org/lrima/laop/core/LaopGraphical.java
@@ -2,11 +2,7 @@ package org.lrima.laop.core;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 
 /**
  * Launch the LAOP platform with a graphical interface
@@ -16,21 +12,10 @@ import javafx.stage.Stage;
 public class LaopGraphical extends Application {
     //TODO: Pouvoir changer entre different stage (settings, simulation et conclusion)
 
+    private Scene currentScene;
+
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Configuration");
-
-        BorderPane pane = new BorderPane();
-        Scene scene = new Scene(pane, 500, 500);
-        stage.setScene(scene);
-
-        Text text = new Text("Bonjour");
-        pane.setCenter(text);
-
-        Line line = new Line(0, 0, 200, 200);
-
-        pane.getChildren().add(line);
-
 
         stage.show();
     }
---
+++ src/main/java/org/lrima/laop/scenes/ConfigurationScene.java
@@ -0,0 +1,13 @@
+package org.lrima.laop.scenes;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class ConfigurationScene extends Scene {
+
+    public ConfigurationScene(Parent root) {
+        super(root);
+    }
+}
