f17bd7763dfd1c17d83463309276039d47f66bf4
Clement <clement.bisaillon@hotmail.com>
2019-03-16 18:12:07 -0400

    Error interseptor

--- src/main/java/org/lrima/laop/graphics/panels/ConsolePanel.java
+++ src/main/java/org/lrima/laop/graphics/panels/ConsolePanel.java
@@ -5,11 +5,13 @@ import java.time.format.DateTimeFormatter;
 
 import org.lrima.laop.controller.PrintInterseptor;
 
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -21,6 +23,7 @@ import javafx.scene.text.TextFlow;
 public class ConsolePanel extends VBox {
 
 	private ScrollPane scrollPane;
+	private final int MAX_WIDTH = 200;
 	
 	public ConsolePanel() {
 		super();
@@ -32,11 +35,14 @@ public class ConsolePanel extends VBox {
         this.scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         
         this.setStyle("-fx-background-color: rgba(255, 255, 255, 0.5)");
+        this.setPrefWidth(MAX_WIDTH);
         
         //Todo: Ne fonctionne pas. Voir general.css
         this.getStyleClass().add("console-panel");
         
         this.setOut();
+        
+        System.err.println("ERROR TEST ASDFASDFASDFASDFASDFA");
 	}
 	
 	/**
@@ -44,20 +50,31 @@ public class ConsolePanel extends VBox {
 	 */
 	private void setOut() {
 		System.setOut(new PrintInterseptor(System.out, (s)->{
-            double oldValue = scrollPane.getVvalue();
-
             String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
             Text prefix = new Text(String.format("[%s] ", time));
             prefix.setStyle("-fx-font-weight: bold;");
             Text suffix = new Text(s);
 
             TextFlow textFlow = new TextFlow(prefix, suffix);
-            textFlow.setMaxWidth(200);
+            textFlow.setMaxWidth(MAX_WIDTH);
             this.getChildren().add(textFlow);
 
             scrollPane.layout();
+        }));
+		
+		System.setErr(new PrintInterseptor(System.err, (s)->{
+            String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            Text prefix = new Text(String.format("ERROR at [%s] ", time));
+            prefix.setFill(Color.RED);
+            prefix.setStyle("-fx-font-weight: bold;");
+            Text suffix = new Text(s);
+            suffix.setFill(Color.RED);
 
-            if(oldValue == 1.0) scrollPane.setVvalue(1.0);
+            TextFlow textFlow = new TextFlow(prefix, suffix);
+            textFlow.setMaxWidth(MAX_WIDTH);
+            this.getChildren().add(textFlow);
+
+            scrollPane.layout();
         }));
 	}
 
