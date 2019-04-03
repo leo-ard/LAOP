f7b3956e39ca3ccd9cea13f79bcd0efdaa1e53c4
Clement <clement.bisaillon@hotmail.com>
2019-04-03 18:41:36 -0400

    View algorithm more information

--- src/main/java/org/lrima/laop/ui/controllers/ViewAlgorithmController.java
+++ src/main/java/org/lrima/laop/ui/controllers/ViewAlgorithmController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import org.lrima.laop.utils.lasp.beans.algorithm.AlgorithmBean;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,10 +22,17 @@ public class ViewAlgorithmController implements Initializable {
 	@FXML Label algorithmName;
 	@FXML JFXButton backButton;
 	@FXML BorderPane root;
+	@FXML JFXTextArea descriptionTextArea;
+	
+	//Author
+	@FXML Label authorName;
 	
 	
 	public void initData(StackPane parent, AlgorithmBean algorithm) {
 		this.algorithmName.setText(algorithm.getTitle());
+		this.descriptionTextArea.setText(algorithm.getDescription());
+		this.authorName.setText(algorithm.getUser().getName());
+		
 		this.backButton.setOnMouseClicked((event) -> {
 			parent.getChildren().remove(root);
 		});
